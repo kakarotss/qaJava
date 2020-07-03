443ebab4e91aca35fd85aa6d191ac37b08df60ab
Arbab Ahmed <Arbab.Ahmed@academytrainee.com>
2020-07-03 10:07:49 +0100

    lambda hellow world

---
+++ exBook/src/main/java/lambda/lambda.java
@@ -0,0 +1,29 @@
+package lambda;
+
+public class lambda {
+
+	public lambda() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		lambda lambda = new lambda();
+		
+		   HelloOperation printHello = ( String a) -> a+" World";
+		   
+		   
+		   
+		   System.out.println(lambda.operate("Hello", printHello));
+		   
+
+	}
+	 interface HelloOperation {
+	      String operation(String a);
+	   } 
+	 
+	  private String operate(String a, HelloOperation mathOperation) {
+	      return mathOperation.operation(a);
+	   }
+
+}
