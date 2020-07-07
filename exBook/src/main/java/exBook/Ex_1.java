47d13d6a63d6f88d9ace4cc3408b07483ab4b82d
Arbab Ahmed <Arbab.Ahmed@academytrainee.com>
2020-07-07 10:17:08 +0100

    mismatch exception added to calculator

--- exBook/src/main/java/exBook/Ex_1.java
+++ exBook/src/main/java/exBook/Ex_1.java
@@ -1,10 +1,11 @@
 package exBook;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//public class Ex_1 {
-	
-	//EX 1
+public class Ex_1 {
+
+	// EX 1
 //	public static String thatsMyName(String name) {
 //		String yourName = name;
 //		return yourName;
@@ -21,90 +22,96 @@ import java.util.Scanner;
 //		in.close();
 //		System.out.println(thatsMyName("You are "+name));
 //	}
-	
-//	public static int addition(int number, int number2) {
-//		return number + number2;
-//		
-//	}
-//	
-//	public static int subtraction(int number, int number2) {
-//		return number - number2;
-//		
-//	}
-//	
-//	public static int division(int number, int number2) {
-//		return number/number2;
-//		
-//	}
-//	
-//	public static int multiplication(int number, int number2) {
-//		return number * number2;
-//		
-//	}
-//	
-//	public static void main(String[] args) {
-//		//EX 2 modded
-//		System.out.println("--------------------------");
-//		System.out.println("CALCULATOR");
-//		System.out.println("--------------------------");
-//		
-//		System.out.println("Enter an option (1-4)");
-//		System.out.println("1- Addition");
-//		System.out.println("2 - Subtraction");
-//		System.out.println("3 - Division");
-//		System.out.println("4 - Multiplication");
-//		
-//		int options;
-//		Scanner in = new Scanner(System.in);
-//		options = in.nextInt();
-//		int num1;
-//		int num2;
-//		
-//		
-//		switch (options) {
-//		case 1:
-//			System.out.println("Addition- enter the first number ");
-//			num1 = in.nextInt();
-//			System.out.println("Addition- enter the second number ");
-//			num2 = in.nextInt();
-//			in.close();
-//			
-//			System.out.println("Your answer is: "+addition(num1,num2));
-//			break;
-//			
-//		case 2:
-//			System.out.println("Subtraction- enter the first number ");
-//			num1 = in.nextInt();
-//			System.out.println("Subtraction- enter the second number ");
-//			num2 = in.nextInt();
-//			
-//			System.out.println("Your answer is: "+subtraction(num1,num2));
-//			in.close();
-//			break;
-//			
-//		case 3:
-//			System.out.println("Division- enter the first number ");
-//			num1 = in.nextInt();
-//			System.out.println("Division- enter the second number ");
-//			num2 = in.nextInt();
-//			in.close();
-//			
-//			System.out.println("Your answer is: "+division(num1,num2));
-//			in.close();
-//			break;
-//			
-//		case 4:
-//			System.out.println("Multiplication- enter the first number ");
-//			num1 = in.nextInt();
-//			System.out.println("Multiplication- enter the second number ");
-//			num2 = in.nextInt();
-//			
-//			System.out.println("Your answer is: "+multiplication(num1,num2));
-//			in.close();
-//			break;
-//			
-//		}
-	
+
+	public static int addition(int number, int number2) {
+		return number + number2;
+
+	}
+
+	public static int subtraction(int number, int number2) {
+		return number - number2;
+
+	}
+
+	public static int division(int number, int number2) {
+		return number / number2;
+
+	}
+
+	public static int multiplication(int number, int number2) {
+		return number * number2;
+
+	}
+
+	public static void main(String[] args) {
+		// EX 2 modded
+		System.out.println("--------------------------");
+		System.out.println("CALCULATOR");
+		System.out.println("--------------------------");
+
+		System.out.println("Enter an option (1-4)");
+		System.out.println("1- Addition");
+		System.out.println("2 - Subtraction");
+		System.out.println("3 - Division");
+		System.out.println("4 - Multiplication");
+
+		int options;
+		Scanner in = new Scanner(System.in);
+		try {
+			options = in.nextInt();
+
+			int num1;
+			int num2;
+
+			switch (options) {
+			case 1:
+				System.out.println("Addition- enter the first number ");
+				num1 = in.nextInt();
+				System.out.println("Addition- enter the second number ");
+				num2 = in.nextInt();
+				in.close();
+
+				System.out.println("Your answer is: " + addition(num1, num2));
+				break;
+
+			case 2:
+				System.out.println("Subtraction- enter the first number ");
+				num1 = in.nextInt();
+				System.out.println("Subtraction- enter the second number ");
+				num2 = in.nextInt();
+
+				System.out.println("Your answer is: " + subtraction(num1, num2));
+				in.close();
+				break;
+
+			case 3:
+				System.out.println("Division- enter the first number ");
+				num1 = in.nextInt();
+				System.out.println("Division- enter the second number ");
+				num2 = in.nextInt();
+				in.close();
+
+				System.out.println("Your answer is: " + division(num1, num2));
+				in.close();
+				break;
+
+			case 4:
+				System.out.println("Multiplication- enter the first number ");
+				num1 = in.nextInt();
+				System.out.println("Multiplication- enter the second number ");
+				num2 = in.nextInt();
+
+				System.out.println("Your answer is: " + multiplication(num1, num2));
+				in.close();
+				break;
+
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid input, shutting down application");
+		}
+
+	}
+
 //	public static void main(String[] args) {
 //		String[] unit = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 //		
@@ -138,4 +145,4 @@ import java.util.Scanner;
 //		}
 //	}
 //
-//}
+}
