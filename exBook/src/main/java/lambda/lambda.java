f527013bef048842476d2031317de303b5d22e3b
Arbab Ahmed <Arbab.Ahmed@academytrainee.com>
2020-07-07 10:03:37 +0100

    week 3 ex completed

--- exBook/src/main/java/lambda/lambda.java
+++ exBook/src/main/java/lambda/lambda.java
@@ -9,21 +9,19 @@ public class lambda {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		lambda lambda = new lambda();
-		
-		   HelloOperation printHello = ( String a) -> a+" World";
-		   
-		   
-		   
-		   System.out.println(lambda.operate("Hello", printHello));
-		   
+
+		HelloOperation printHello = (String a) -> a + " World";
+
+		System.out.println(lambda.operate("Hello", printHello));
 
 	}
-	 interface HelloOperation {
-	      String operation(String a);
-	   } 
-	 
-	  private String operate(String a, HelloOperation mathOperation) {
-	      return mathOperation.operation(a);
-	   }
+
+	interface HelloOperation {
+		String operation(String a);
+	}
+
+	private String operate(String a, HelloOperation mathOperation) {
+		return mathOperation.operation(a);
+	}
 
 }
